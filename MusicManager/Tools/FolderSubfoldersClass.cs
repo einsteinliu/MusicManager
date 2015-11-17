47853fb358fa79b55c935f55cbe943dc174d75cd
DanqingYANG <lenayoungydq@gmail.com>
2015-11-17 21:40:38 +0100

    Tools

---
+++ MusicManager/Tools/FolderSubfoldersClass.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tools
+{
+    //按照选择的文件夹路径生成文件夹树, 
+    //输入: 默认数据库内容 , 鼠标选择内容
+    public class SubfoldersClass
+    {
+        public void test()
+        {
+            //输出文件夹和子文件夹的dictionary
+            for (int i = 0; i < SubFolderDic.Count; i++)
+            {
+                Console.WriteLine(SubFolderDic.Keys.ElementAt<string>(i));
+                Console.WriteLine();
+                for (int j = 0; j < SubFolderDic.Values.ElementAt<List<string>>(i).Count; j++)
+                {
+                    Console.WriteLine(SubFolderDic.Values.ElementAt<List<string>>(i)[j]);
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            //输出文件夹和子文件们的dictionary
+            for (int i = 0; i < SubFilePathsDic.Count; i++)
+            {
+                Console.WriteLine(SubFilePathsDic.Keys.ElementAt<string>(i));
+                Console.WriteLine();
+                for (int j = 0; j < SubFilePathsDic.Values.ElementAt<List<string>>(i).Count; j++)
+                {
+                    Console.WriteLine(SubFilePathsDic.Values.ElementAt<List<string>>(i)[j]);
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+
+
+        }
+
+        public SubfoldersClass(List<string> folderPathList)
+        {
+
+            for (int i = 0; i < folderPathList.Count; i++)
+            {
+                string key = folderPathList[i];
+                DirectoryInfo rootDirectoryInfo = new DirectoryInfo(folderPathList[i]);
+                List<string> subFolderPathsList = new List<string>();
+                DirectoryInfo directoryInfo = new DirectoryInfo(folderPathList[i]);
+                List<string> subdirectoryEntries = new List<string>();
+                List<string> sl = new List<string>();
+                //递归得到下级的所有文件 用于传递给FileTypeFilter
+                subFolderPathsList = subFolders(directoryInfo, /*subdirectoryEntries,*/sl);
+                _subFolderDic[key] = subFolderPathsList;
+                //得到这些根目录下的文件们。初始化_subFilePathsDic
+                SetSubfilesDic();
+            }
+        }
+
+        private Dictionary<string, List<string>> _subFolderDic = new Dictionary<string,List<string>>();
+        public Dictionary<string, List<string>> SubFolderDic
+        {
+            get
+            {
+                return _subFolderDic;
+            }
+        }
+
+        private Dictionary<string, List<string>> _subFilePathsDic = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> SubFilePathsDic
+        {
+            get
+            {
+                return _subFilePathsDic;
+            }
+        }
+
+        //得到一些根目录下所有子目录内的文件集合,Dictionary string是根目录, List<string> 内为其内部的所有文件路径。
+        public Dictionary<string, List<string>> SetSubfilesDic()
+        {
+            //Dictionary<string, List<string>> _subFilePathsDic = new Dictionary<string, List<string>>();
+            //处理单个根目录
+            for (int i = 0; i < _subFolderDic.Count; i++)
+            {
+                List<string> filenames = new List<string>();
+
+                //添加根目录下的文件们
+                string[] targetFolderFiles = Directory.GetFiles(_subFolderDic.Keys.ElementAt<string>(i));   
+                filenames.AddRange(targetFolderFiles.ToList());
+                //添加子目录下的所有文件们
+                for (int j = 0; j < _subFolderDic.Values.ElementAt<List<string>>(i).Count; j++)
+                {
+                    string folderPath = _subFolderDic.Values.ElementAt<List<string>>(i)[j];
+                    string[] files = Directory.GetFiles(folderPath);
+                    filenames.AddRange(files.ToList());            
+                }
+
+                _subFilePathsDic[_subFolderDic.Keys.ElementAt<string>(i)]=filenames;
+            }
+            return _subFilePathsDic;
+        }
+
+
+        public List<string> subFolders(DirectoryInfo targetDirectory, List<string> SubFolderList)
+        {
+            //
+            DirectoryInfo[] subdirs = targetDirectory.GetDirectories();
+            for (int i = 0; i < subdirs.Length; i++)
+            {
+                SubFolderList.Add(subdirs[i].FullName); ;
+            }
+            
+            foreach (var di in subdirs)
+            {
+                subFolders(di, SubFolderList);
+            }
+            return SubFolderList;
+        }
+        
+        private bool hasSubfolder(string path)
+        {
+            IEnumerable<string> subfolders = Directory.EnumerateDirectories(path);
+            return subfolders != null && subfolders.Any();
+        }
+
+        //
+        //
+
+        //输出的targetFolderPath
+
+
+
+        private string _initIndexFile;
+        public string InitIndexFile
+        {
+            set
+            {
+                _initIndexFile = value;
+            }
+            get
+            {
+                return _initIndexFile;
+            }
+        }
+
+
+        private List<string> _targetFolderPaths;
+        public List<string> TargetFolderPaths
+        {
+            get
+            {
+                return _targetFolderPaths;
+            }
+            //
+            set
+            {
+                _targetFolderPaths = value;
+            }
+        }
+
+    }
+
+
+
+
+    //按照输入的文件后缀名 筛选 指定文件夹内 拥有相应后缀名的文件
+    //输入:string fileTypes, targetFolder
+    //输出:List<string> FilePaths, string Key
+    public class FileTypeFilter
+    {
+        public void test()
+        {
+            Console.WriteLine(FilePaths);
+        }
+
+        public FileTypeFilter(List<string> fileTypesList, string targetDirectory)
+        {
+            setFileNames(fileTypesList, targetDirectory);
+        }
+
+
+
+
+        private List<string> _fileNames;
+        public List<string> FileNames
+        {
+            get
+            {
+                return _fileNames;
+            }
+        }
+
+        private int _count = 0;
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        private string _key;
+        public string Key
+        {
+            set
+            {
+                _key = value;
+            }
+            get
+            {
+                return _key;
+            }
+        }
+
+        private List<string> _filePaths;
+        public List<string> FilePaths
+        {
+            get
+            {
+                return _filePaths;
+            }
+        }
+
+        public void setFileNames(List<string> fileTypeInput, string targetDirectory)
+        {
+            DirectoryInfo di = new DirectoryInfo(targetDirectory);
+            List<string> fn = new List<string>();
+            List<string> fp = new List<string>();
+            for (int i = 0; i < fileTypeInput.Count; i++)
+            {
+                //
+                string key = fileTypeInput[i];
+                //
+                foreach (var fi in di.GetFiles(key))
+                {
+                    Console.WriteLine(fi.DirectoryName);
+                    fn.Add(fi.Name);
+                    fp.Add(fi.FullName);
+                    _count++;
+                }
+            }
+            _fileNames = fn;
+            _filePaths = fp;
+        }
+        //
+    }
+
+
+
+    //这部分需要存储
+    public class InputFileTypes
+    {
+        public InputFileTypes(string input)
+        {
+
+            setFileTypes(input);
+        }
+
+        private List<string> _fileTypesList;
+        public List<string> FileTypesList
+        {
+            get
+            {
+                return _fileTypesList;
+            }
+        }
+        public void setFileTypes(string line)
+        {
+            List<string> fileTypesList = new List<string>();
+            string[] linesplit = line.Split(',', ' ');
+            for (int i = 0; i < linesplit.Length; i++)
+            {
+                if (linesplit[i] != "")
+                {
+                    fileTypesList.Add("*." + linesplit[i]);
+                }
+            }
+            _fileTypesList = fileTypesList;
+        }
+    }
+}
---
+++ MusicManager/Tools/OpenMusicClass.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tools
+{
+    public class OpenMusicClass
+    {
+        //输入: 文件夹列表 
+        //功能: 用airPlay打开文件夹列表 播放音乐 获取播放器.exe位置
+        //输出: 下层音乐文件列表。文件夹名、文件名、格式、时长(待定)
+
+        public OpenMusicClass(List<string> selectedFolderPath)
+        {
+
+        }
+
+        //文件夹目录列表
+        private List<string> _folderPaths = new List<string>();
+        public List<string> FolderPath 
+        {
+            get
+            {
+                return _folderPaths;
+            } 
+            set
+            {
+
+            }
+        }
+
+        //音乐文件列表
+        private List<MusicFile> _musicFiles = new List<MusicFile>();
+        public List<MusicFile> MusicFiles { get; set; }
+
+
+
+
+        //类测试函数
+        public void test()
+        {
+            string folder;
+            string playHistoryPath = @"F:\music\AIRPLAY_CONFIG\LOCAL\audition.locallist";
+            folder = getFolder();
+            clearPlayHistory(playHistoryPath);
+            openMusic(folder);
+        }
+
+
+        public string getFolder()
+        {
+            string folder = @"F:\music\Bach\Bach.-.[Goldberg.Variations(Walcha.EMI.Angle)].专辑.(Flac)";
+            Console.WriteLine("Open the folder: " + folder);
+            return folder;
+        }
+
+        public string[] getFiles(string folder)
+        {
+            string[] files = Directory.GetFiles(folder);
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i]);
+            }
+
+            return files;
+        }
+
+        public void openMusic(string folder)
+        {
+            string filepath = "\"" + folder + "\"";
+            Process pro = Process.Start(@"F:\music\AIRPLAY.exe", folder);
+            Thread.Sleep(800);
+            pro.Close();
+        }
+
+        ////判断是否为音乐格式
+        //public bool isType(List<string> spcMusicTypes)
+        //{
+        //    bool b = false;
+        //    for (int i = 0; i < spcMusicTypes.Count; i++)
+        //    {
+        //        b = (FileType == spcMusicTypes[i] ? true : false);
+        //    }
+        //    return b;
+        //}
+
+        public void clearPlayHistory(string filename)
+        {
+
+            File.Delete(filename);
+        }
+
+    }
+
+    //DataGrid内需要的信息
+    //输入:单个文件地址 
+    //输出: 音乐文件名, 文件夹名、文件名、格式、时长(待定)
+    public class MusicFile
+    {
+
+        public MusicFile(string fp)
+        {
+            FilePath = fp;
+            FileName = Path.GetFileNameWithoutExtension(FilePath);
+            FileType = Path.GetExtension(FilePath);
+            _musicDuration = setMusicDuraion();
+            //MusicDuration = 
+        }
+
+        public void test()
+        {
+            TagLib.File tl = TagLib.File.Create(FilePath);
+            Console.WriteLine(tl.Properties.Duration.ToString());
+
+            Console.WriteLine(FilePath);
+            Console.WriteLine(FileName);
+            Console.WriteLine(FileType);
+           //Console.WriteLine(FileInformation);
+            Console.WriteLine(MusicDuration);
+
+        }
+
+        private string _filePath;
+        public string FilePath
+        {
+            get
+            {
+                return _filePath;
+            }
+            set 
+            {
+                _filePath = value;
+            }
+        }
+
+        //private FileInfo _fileInformation;
+        //public FileInfo FileInformation
+        //{
+        //    get
+        //    {
+        //        return _fileInformation;
+        //    }
+        //    set
+        //    {
+        //        _fileInformation = value;
+        //    }
+        //}
+
+        private string _fileName;
+        public string FileName
+        {
+            get
+            {
+                return _fileName;
+            }
+            set
+            {
+                _fileName = value;
+            }
+        }
+
+        private string _fileType;
+        public string FileType
+        {
+            get
+            {
+                return _fileType;
+            }
+            set
+            {
+                _fileType = value;
+            }
+        }
+
+        private string _musicDuration;
+        public string MusicDuration
+        {
+            get
+            {
+                return _musicDuration;
+            }
+        }
+
+        public string setMusicDuraion()
+        {
+            string d = null;
+            TagLib.File tl = TagLib.File.Create(FilePath);
+            d = tl.Properties.Duration.ToString();
+            return d;
+        }
+    }
+}
